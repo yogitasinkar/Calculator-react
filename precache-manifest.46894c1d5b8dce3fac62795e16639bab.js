d057d95c978a3395921ddf048b2f5a2e7e7a1100
yogita <yogita.sinkar5@gmail.com>
2020-06-25 23:17:05 +0530

    Updates

---
+++ precache-manifest.46894c1d5b8dce3fac62795e16639bab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "314da7f2234f9ef4382e02b840987821",
+    "url": "/yogitasinkar/Calculator-react/index.html"
+  },
+  {
+    "revision": "9e768ca17483d9b3e22e",
+    "url": "/yogitasinkar/Calculator-react/static/css/main.d27a59ef.chunk.css"
+  },
+  {
+    "revision": "114bbaea14242877659e",
+    "url": "/yogitasinkar/Calculator-react/static/js/2.6912228e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/yogitasinkar/Calculator-react/static/js/2.6912228e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e768ca17483d9b3e22e",
+    "url": "/yogitasinkar/Calculator-react/static/js/main.b9c1b9a1.chunk.js"
+  },
+  {
+    "revision": "c850de28793314237ded",
+    "url": "/yogitasinkar/Calculator-react/static/js/runtime-main.5d6c8457.js"
+  }
+]);
